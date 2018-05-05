7b22e64f5e84d571c03946b948509f170c8d4feb
poltorres0999 <eichgammer@gmail.com>
2018-05-05 19:11:10 +0200

    Machine Composite repair/setBroken
    Just implemented the methods, need to see how to manage the addition of observers, and update trackBrokenComponents list to keep it updated in case any component uses it's method setBroken

--- src/MachineComposite.java
+++ src/MachineComposite.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 public class MachineComposite extends MachineComponent{
     private List<MachineComponent> components = new ArrayList<>();
+    private List<MachineComponent> trackBrokenComponents = new ArrayList<>();
 
     public void addComponent(MachineComponent mc) {
         this.components.add(mc);
@@ -10,12 +11,22 @@ public class MachineComposite extends MachineComponent{
 
     @Override
     public void setBroken() {
-
+        if (!isBroken()) {
+            this.broken = true;
+            notifyObservers();
+        }
     }
 
     @Override
     public void repair() {
-
+        if (isBroken()) {
+            for (MachineComponent mc: trackBrokenComponents) {
+                mc.repair();
+            }
+            trackBrokenComponents.clear();
+            this.broken = false;
+            notifyObservers();
+        }
     }
 
     @Override
