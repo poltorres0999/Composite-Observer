21f317c28f05f729ae695001a9d02a8b65e3cb5a
poltorres0999 <eichgammer@gmail.com>
2018-05-15 23:43:30 +0200

    Refactor MachineTest

--- src/test/MachineTest.java
+++ src/test/MachineTest.java
@@ -1,26 +1,40 @@
 package test;
 
 import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
 import machine.*;
 
 public class MachineTest {
+
+    private Machine machine;
+
+    @Before
+    public void initialize () {
+
+        machine = new Machine();
+    }
+
     @Test
-    public void testBrokenMachine() {
+    public void testMachine() {
+        Machine machine = new Machine();
+        assertFalse(machine.isBroken());
+    }
+
+    @Test
+    public void testSetBrokenMachine() {
         Machine machine = new Machine();
         machine.setBroken();
         assertTrue(machine.isBroken());
     }
+
     @Test
-    public void testNoBrokenMachine() {
-        Machine machine = new Machine();
-        assertFalse(machine.isBroken());
-    }
-    @Test
-    public void testMachineRepair() {
+    public void testRepairMachine() {
         Machine machine = new Machine();
         machine.setBroken();
         machine.repair();
         assertFalse(machine.isBroken());
     }
+    
 }
